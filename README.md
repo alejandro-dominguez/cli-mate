ea15266b34df1ad6cbdb93e386f2a60f9886d45c
alejandro-dominguez <alejandro.dominguez1@live.com>
2022-10-05 14:21:54 -0300

    renderizar datos de API, control de errores, mostrar fecha y fondo animado

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ Cli-mate es una web donde puedes chequear el clima de tu ciudad y del lugar que 
 En este proyecto he utilizado JQuery, LESS, HTML y CSS.
 
 # Frameworks
-Bootstrap y Sweet Alert.
+Bootstrap.
+
+# Conceptos trabajados
+Algunas de las habilidades que he trabajado en este proyecto son:
+- Recuperar datos de una API de clima para generar el contenido de la página y las interacciones.
+- Animar el fondo de la página utilizando una animación de CSS con gradientes.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const url = "https://api.openweathermap.org/data/2.5/weather";
 const appId = "a20cca282f35b96f384a5385a3120a48";
 
-// checking for geolocation and calling form submit event // 
+// checking for geolocation, calling form submit event and getting current date // 
 
 $(document).ready(function() {
     navigator.geolocation ? navigator.geolocation.getCurrentPosition(function(position){
@@ -10,7 +10,31 @@ $(document).ready(function() {
     $("#form").submit(function(event, appId) { 
         search(event, appId)
     })
+
+    getDate()
 });
+
+// get current date //
+
+const getDate = () => {
+    let current = $.now()
+    let maxDate = new Date(current)
+    let currentDate = maxDate.getDate()
+    let currentMonth = maxDate.getMonth() + 1
+    let currentYear = maxDate.getFullYear()
+
+    $("#date").text("")
+    $("#current-date").text("")
+    
+    renderDate(currentDate, currentMonth, currentYear)
+}
+
+// render current date //
+
+const renderDate = (date, month, year) => {
+    $("#date").text("Fecha:")
+    $("#current-date").text(`${date}/${month}/${year}`)
+}
 
 // API calls //
 
@@ -25,7 +49,7 @@ const defaultWeather = (position) => {
         data: { appid: `${appId}`,
                 lat: `${lat}`,
                 lon: `${lon}`,
-                units: "metric",
+                units: "metric"
             }
     })
 
@@ -38,19 +62,45 @@ const defaultWeather = (position) => {
     })
 }
 
+// render geolocation call //
+
 const renderWeather = (response) => {
     let cityName = response.name
     cityName = `${cityName}, ${response.sys.country}`
     let imgUrl = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
     let cityTemp = response.main.temp
-    let cityWeather = response.weather[0].main
+    let cityWeather = response.weather[0].description
+    let cityHumidity = response.main.humidity
+    let cityWind = response.wind.speed
 
-    $("#city-name").text(cityName)
+    $("#city-name").text(`El clima en ${cityName}`)
     $("#weather-img").attr("src", imgUrl)
     $("#weather-img").attr("alt", cityWeather)
     $("#city-temp").text(`${cityTemp} C°`)
     $("#city-weather").text(cityWeather)
+    $("#city-humidity").text(`Humedad: ${cityHumidity}%`)
+    $("#city-wind").text(`Viento: ${cityWind} Km/h`)
 }
+
+// geolocation call error //
+
+const defaultError = () => {
+    $("#city-name").removeClass("city-name")
+    $("#city-name").addClass("error")
+    $("#city-name").text("¡Ups! Lo sentimos mucho, ha ocurrido un problema al conectarnos con el servidor. Intenta refrescar la página.")
+    $("#weather-img").removeClass("weather-img")
+    $("#weather-img").addClass("img-error")
+    $("#weather-img").attr("src", "./images/error.svg")
+    $("#weather-img").attr("alt", "error al cargar")
+    $("#city-temp").text("")
+    $("#city-weather").text("")
+    $("#city-humidity").text("")
+    $("#city-wind").text("")
+    $("#date").text("")
+    $("#current-date").text("")
+}
+
+// search call //
 
 const search = (event) => {
     let request
@@ -60,6 +110,8 @@ const search = (event) => {
     $("#weather-img").attr("alt", "")
     $("#city-temp").text("")
     $("#city-weather").text("")
+    $("#city-humidity").text("")
+    $("#city-wind").text("")
 
     request = $.ajax({
         url: `${url}`,
@@ -86,20 +138,33 @@ const renderResult = (response) => {
     cityName = `${cityName}, ${response.sys.country}`
     let imgUrl = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
     let cityTemp = response.main.temp
-    let cityWeather = response.weather[0].main
+    let cityWeather = response.weather[0].description
+    let cityHumidity = response.main.humidity
+    let cityWind = response.wind.speed
 
     $("#city-name").text(cityName)
     $("#weather-img").attr("src", imgUrl)
     $("#weather-img").attr("alt", cityWeather)
     $("#city-temp").text(`${cityTemp} C°`)
     $("#city-weather").text(cityWeather)
+    $("#city-humidity").text(`Humedad: ${cityHumidity}%`)
+    $("#city-wind").text(`Viento: ${cityWind} Km/h`)
 }
 
 // search error //
 
 const searchError = () => {
-    $("#city-name").text("Por favor vuelve a intentarlo, los datos ingresados son incorrectos!")
-    $("#weather-img").attr("src", "") // "src", "./images/wrong-input.svg"
+    $("#city-name").removeClass("city-name")
+    $("#city-name").addClass("error")
+    $("#city-name").text("Por favor vuelve a intentarlo !Los datos ingresados son incorrectos!")
+    $("#weather-img").removeClass("weather-img")
+    $("#weather-img").addClass("img-error")
+    $("#weather-img").attr("src", "./images/error.svg")
+    $("#weather-img").attr("alt", "error al cargar")
     $("#city-temp").text("")
     $("#city-weather").text("")
+    $("#city-humidity").text("")
+    $("#city-wind").text("")
+    $("#date").text("")
+    $("#current-date").text("")
 }
